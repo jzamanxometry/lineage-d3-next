db9c58ffa16b174a1c1cbeec19299b36a785bb9c
jzamanxometry <83312507+jzamanxometry@users.noreply.github.com>
2022-01-28 15:00:00 -0500

    added demo to project

--- pages/lineage-d3/index.js
+++ pages/lineage-d3/index.js
@@ -15,7 +15,7 @@ export default function LineageD3() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main >
+            <main className={styles.main}>
                 <h1>
                     Data Lineage using D3
                 </h1>
